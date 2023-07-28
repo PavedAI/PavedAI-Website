48083fb97d45bef586c5958802ceb6f24c2173b9
VA_24 <24agnihotriv@smtexas.org>
2023-07-28 13:55:16 -0500

    init home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+// import About from './pages/About';
+// import Products from './pages/Products';
+// import Investment from './pages/Investment';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />}/>
+          {/* <Route path='/about' exact component={About}/>
+          <Route path='/products' exact component={Products}/>
+          <Route path='/investment' exact component={Investment}/> */}
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../App.css';
+import './Home.css';
+
+function Home(){
+    return(
+        <div className='main-container'>
+            <h1>
+                Clearing the world's cracked concrete with robotics and a little bit of computer vision sprinkled in.
+            </h1>
+            
+            <h2>
+                In 2023, PavedAI hopes to launch its first robot - nicknamed "P1" for Prototype 1 - in an attempt to solve
+            </h2>
+            <p>
+                At PavedAI, we're developing a robot that can autonomous detect and fill cracks in concrete sidewalks, pavement, and roads. Take a look at how we're doing so: 
+            </p>
+        </div>
+    )
+}
+
+export default Home;
