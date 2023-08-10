d601a2433fe328361ebbf4721cef51ee835472c7
VA_24 <24agnihotriv@smtexas.org>
2023-08-10 12:09:42 -0500

    added 404 page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Product from './pages/Products';
 import Investment from './pages/Investment';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import PageNotFound from './pages/PageNotFound'
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 
 function App() {
   return (
@@ -14,9 +15,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path='/' element={<Home />}/>
-          <Route exact path='/about' element={<About />}/>
+          <Route exact path='/about' element={<About  />}/>
           <Route exact path='/products' element={<Product />}/>
           <Route exact path='/invest' element={<Investment />}/>
+          <Route path="/404" element={<PageNotFound />} />
+          <Route path="*" element={<Navigate to="/404" />} />
         </Routes>
       </Router>
     </>
---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PageNotFound.css';
+
+function PageNotFound(){
+    return(
+        <div className='NotFoundContainer'>
+            <h1>We're sorry, the page you're looking for isn't available or may currently be under construction</h1>
+            <p>Check back later for more 😈</p>
+        </div>
+    )
+}
+
+export default PageNotFound;
